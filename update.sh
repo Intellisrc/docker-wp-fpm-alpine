dcdf6a5ad6b73289c819eb7c9fdfdf822c454928
A.Lepe <lepe@support.ne.jp>
2023-03-06 12:28:26 +0900

    Updated script

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+ver=$(grep "FROM" ./Dockerfile | awk -F':' '{ print $2 }')
+if [[ $ver == "" ]]; then
+	echo "Version not found"
+	exit
+fi
+echo "VERSION: $ver"
 docker build -t wp-fpm-alpine .
-docker tag wp-fpm-alpine:latest intellisrc/wp-fpm-alpine:latest
-docker push intellisrc/wp-fpm-alpine:latest
+docker tag wp-fpm-alpine:latest intellisrc/wp-fpm-alpine:$ver
+docker push intellisrc/wp-fpm-alpine:$ver
