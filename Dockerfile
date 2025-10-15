93be935a3e48392b3e33f2390709246a88567d0f
A.Lepe <lepe@support.ne.jp>
2025-10-15 16:54:58 +0900

    Reverted using lighttpd start.sh as this version is not using intellisrc:lighttpd image

--- Dockerfile
+++ Dockerfile
@@ -39,4 +39,4 @@ RUN mkdir -p /var/log/lighttpd/ && \
 	ln -s /etc/php$PHP_VER /etc/php 
 
 WORKDIR /var/www
-CMD ["wp-start.sh"]
+CMD ["start.sh"]
--- image/start.sh
+++ image/start.sh
@@ -118,5 +118,11 @@ else
 	echo "WP patch for lighttpd not found"
 fi
 
-# Executing PHP start.sh
-/usr/local/bin/start.sh
+# Setting php-fpm config
+fpm_config=/etc/php/php-fpm.d/www.conf
+sed -i "s/PHP_MIN_WORKERS/$PHP_MIN_WORKERS/g" "$fpm_config"
+sed -i "s/PHP_MAX_WORKERS/$PHP_MAX_WORKERS/g" "$fpm_config"
+echo "Starting PHP-FPM...."
+php-fpm -D
+echo "Starting lighttpd...."
+lighttpd -D -f /etc/lighttpd/lighttpd.conf
