c347f12a1a83182bb78100940b70e0e99267f240
A.Lepe <lepe@support.ne.jp>
2026-08-06 14:49:34 +0900

    updated update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,9 @@ if [[ $ver == "" ]]; then
 	exit
 fi
 echo "VERSION: $ver"
-docker build -t wp-fpm-alpine .
-docker tag wp-fpm-alpine:latest intellisrc/wp-fpm-alpine:$ver
-docker push intellisrc/wp-fpm-alpine:$ver
+#docker build -t wp-fpm-alpine .
+#docker tag wp-fpm-alpine:latest intellisrc/wp-fpm-alpine:$ver
+#docker push intellisrc/wp-fpm-alpine:$ver
+
+docker buildx build --load --push -t intellisrc/wp-fpm-alpine:$ver .
 echo "Updated: intellisrc/wp-fpm-alpine:$ver"
